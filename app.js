39a846fdafc4669ce4a10b829e8d7f9824060962
Simon Chauvel <simonchauvel@gmail.com>
2022-08-17 13:29:29 +0200

    functional version of app.js

--- app.js
+++ app.js
@@ -6,3 +6,49 @@ Le modal disparait lorsqu'on recommence le cycle.
 Un deuxième bouton doit servir à masquer le modal.
 
 */
+
+
+/* ****************************************************************
+**************************** VARIABLES ****************************
+**************************************************************** */
+const redDiv = document.querySelector(".red");
+const modalDiv = document.querySelector(".modal");
+const moveDiv = document.querySelector("#moveDiv");
+const hideModal = document.querySelector("#hideModal");
+let clickCounter = 0;
+
+/* ****************************************************************
+************************* EVENT-HANDLERS **************************
+**************************************************************** */
+function showingModal() {
+    modalDiv.style.display = "block";
+};
+
+function hidingModal() {
+    modalDiv.style.display = "none";
+};
+
+moveDiv.addEventListener("click", () => {
+    switch(clickCounter) {
+        case 0:
+            redDiv.style.marginLeft = "250px";
+            clickCounter = 1;
+            hidingModal();
+            break;
+        case 1:
+            redDiv.style.marginTop = "250px";
+            clickCounter = 2;
+            break;
+        case 2:
+            redDiv.style.marginLeft = "0px";
+            clickCounter = 3;
+            break;
+        case 3:
+            redDiv.style.marginTop = "0px";
+            clickCounter = 0;
+            showingModal();
+            break;
+    }
+});
+
+hideModal.addEventListener("click", hidingModal);
